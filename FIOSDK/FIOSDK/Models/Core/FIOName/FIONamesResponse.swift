d2327fc43514b472f30c1d67586a2efb98023de9
Kenneth Rangel <kenneth.rangel@bairesdev.com>
2019-09-20 20:00:39 +0200

    update to api 0.3

--- FIOSDK/FIOSDK/Models/Core/FIOName/FIONamesResponse.swift
+++ FIOSDK/FIOSDK/Models/Core/FIOName/FIONamesResponse.swift
@@ -24,6 +24,7 @@ extension FIOSDK.Responses {
         public struct FIODomainResponse: Codable{
             public let domain: String
             private let _expiration: String
+            public let is_public: Bool
             
             public var expiration: Date{
                 return Date(timeIntervalSince1970: (Double(_expiration) ?? 0))
@@ -32,11 +33,13 @@ extension FIOSDK.Responses {
             enum CodingKeys: String, CodingKey{
                 case domain = "fio_domain"
                 case _expiration = "expiration"
+                case is_public = "is_public"
             }
         }
         
     }
 
+    
     public struct FIOAddressResponse: Codable{
         public let address: String
         private let _expiration: String
